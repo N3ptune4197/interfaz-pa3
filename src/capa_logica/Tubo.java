2e6d9bfff94034aa2766a5c8b33d6578b39fe4fb
Fabrizzio Estela <festelav@unprg.edu.pe>
2026-08-26 14:30:49 -0500

    tubo java

---
+++ src/capa_logica/Tubo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package capa_logica;
+
+/**
+ *
+ * @author NAYR VASQUEZ
+ */
+public class Tubo {
+    private double x;
+    private int anchoTubo = 60;
+    private int altoHueco;
+    private int posicionHueco; // coordenada Y donde comienza el hueco
+    private double velocidad;
+    private boolean superado; // true si el pájaro ya lo pasó (para sumar puntos una sola vez)
+}
